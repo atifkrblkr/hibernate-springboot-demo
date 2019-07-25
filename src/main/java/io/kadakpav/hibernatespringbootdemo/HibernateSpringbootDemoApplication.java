484162b8c1c165366d56dafaaa9fa54b08a4f33f
Atif Karbelkar <atif.krblkr@gmail.com>
2019-07-25 13:35:34 -0400

    Initial commit

---
+++ src/main/java/io/kadakpav/hibernatespringbootdemo/HibernateSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package io.kadakpav.hibernatespringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateSpringbootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/kadakpav/hibernatespringbootdemo/HibernateSpringbootDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package io.kadakpav.hibernatespringbootdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HibernateSpringbootDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
